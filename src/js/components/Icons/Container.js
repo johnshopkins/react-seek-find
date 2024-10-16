94c830332e17acd9833d1e4df08b1211e7f140d6
Jen Wachter <jwachter@jhu.edu>
2024-10-16 10:32:38 -0400

    Gave icons some default styles

--- src/js/components/Icons/Container.js
+++ src/js/components/Icons/Container.js
@@ -1,7 +1,18 @@
 import { Children } from 'react';
 
-export default ({ children, className, style, tooltip, viewBox = '0 0 512 512' }) =>
-  <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox={viewBox} style={style}>
-    {tooltip && <title>{tooltip}</title>}
-    {Children.map(children, child => <>{child}</>)}
-  </svg>
+import './style.scss';
+
+export default ({ children, className, style, tooltip, viewBox = '0 0 512 512' }) => {
+
+  const classes = ['jhu-icon']
+  if (className) {
+    classes.push(className);
+  }
+
+  return (
+    <svg className={classes.join(' ')} xmlns="http://www.w3.org/2000/svg" viewBox={viewBox} style={style}>
+      {tooltip && <title>{tooltip}</title>}
+      {Children.map(children, child => <>{child}</>)}
+    </svg>
+  )
+}
