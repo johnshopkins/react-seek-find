7c33e7c0ee2a94c799af312262ddfeb90f933869
Jen Wachter <jwachter@jhu.edu>
2024-10-17 14:37:47 -0400

    Just use an img tag instead of a canvas now that we're using a separate canvas for the clickabla areas and hints

--- src/js/components/Background/index.js
+++ src/js/components/Background/index.js
@@ -1,37 +1,12 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
 import './style.scss';
 
 export default ({ height, imageSrc, scale, width }) => {
 
-  const ref = useRef(null);
+  const scaledWidth = width * scale;
+  const scaledHeight = height * scale;
 
-  useEffect(() => {
-
-    const context = ref.current.getContext('2d');
-
-    context.clearRect(0, 0, width, height);
-    context.resetTransform();
-    context.scale(scale, scale);
-
-    const image = new Image();
-    image.src = imageSrc;
-    
-    image.onload = () => {
-      context.globalCompositeOperation = 'destination-over';
-      context.drawImage(image, 0, 0, width, height);
-      context.globalCompositeOperation = 'source-over';
-    };
-
-  }, [scale]);
-
-  return (
-    <canvas
-      ref={ref}
-      className="background"
-      height={height}
-      width={width}
-    />
-  )
+  return <img src={imageSrc} width={scaledWidth} height={scaledHeight} />
 
 };
