94389dfc616bee58a4bd31583fdfea2a4dd24452
Jen Wachter <jwachter@jhu.edu>
2024-10-25 10:49:36 -0400

    Added loading icon for background image

--- src/js/components/Background/index.js
+++ src/js/components/Background/index.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoadingIcon from '../Icons/Loading'
 import './style.scss';
 
 /**
  * The background image
  */
-export default function Background({ height, imageSrc, scale, width }) {
+export default function Background({ containerHeight, containerWidth, height, imageSrc, scale, width }) {
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const scaledWidth = width * scale;
   const scaledHeight = height * scale;
 
+  const loadingIconSize = 30;
+
+  const loadingStyle = {
+    height: `${loadingIconSize}px`,
+    left: (containerWidth / 2) - (loadingIconSize / 2),
+    top: (containerHeight / 2) - (loadingIconSize / 2),
+    width: `${loadingIconSize}px`,
+  }
+
   return (
-    <img
-      className="game-background"
-      alt="Seek and find"
-      src={imageSrc}
-      width={scaledWidth}
-      height={scaledHeight}
-    />
+    <>
+      {isLoading && <LoadingIcon className="loading" style={loadingStyle} />}
+      <img
+        className="game-background"
+        alt="Seek and find"
+        src={imageSrc}
+        width={scaledWidth}
+        height={scaledHeight}
+        onLoad={() => setIsLoading(false)}
+        style={{ display: isLoading ? 'none' : 'block' }}
+      />
+    </>
   )
 
 };
