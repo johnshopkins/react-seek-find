cf3fa0155527162ce9af42d5051faac8bb53b5b2
Jen Wachter <jwachter@jhu.edu>
2024-11-04 08:49:41 -0500

    Added dev server settings to dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,6 +11,10 @@ module.exports = {
     // libraryTarget: 'commonjs2',
   },
   devServer: {
+    devServer: {
+      host: '0.0.0.0',
+      port: 8080,
+    },
     static: {
       directory: path.join(__dirname, 'example'),
     }
