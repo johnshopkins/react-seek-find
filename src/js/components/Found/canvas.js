b3bedd780d690cd3ba1188affb1008163250aa42
Jen Wachter <jwachter@jhu.edu>
2024-11-09 09:50:58 -0500

    Size found canvas via css to allow for transitioning

--- src/js/components/Found/canvas.js
+++ src/js/components/Found/canvas.js
@@ -3,27 +3,24 @@ import React, { memo, useEffect, useRef } from 'react';
 export default memo(function Found({ object, height, scale, width }) {
 
   const ref = useRef(null);
-  const classes = useRef(['found']);
 
   useEffect(() => {
 
     const context = ref.current.getContext('2d');
-
-    context.clearRect(0, 0, width, height);
-    context.resetTransform();
-    context.scale(scale, scale);
-
-    // draw found object
     object.create.call(this, context, true);
 
-  }, [classes, height, object, scale, width]);
+  }, [object]);
 
   return (
     <canvas
       ref={ref}
-      className={classes.current.join(' ')}
+      className="found"
       height={height}
       width={width}
+      style={{
+        height: `${height * scale}px`,
+        width: `${width * scale}px`
+      }}
     />
   )
 
