e6bc5b992f2f83fb2545551ec4f7e5e5e9721166
Jen Wachter <jwachter@jhu.edu>
2024-11-13 09:39:06 -0500

    Added a bunch of objects to test file

--- example/js/index.js
+++ example/js/index.js
@@ -1,5 +1,5 @@
 const { createRoot } = ReactDOM;
-import { Game } from '../../src/js/main';
+import { FindableObject, Game } from '../../src/js/main';
 
 import image from '../images/background.png';
 import Beanie from './objects/Beanie';
@@ -11,18 +11,50 @@ import Circle from './objects/Circle';
 const elem = document.getElementById('root');
 const root = createRoot(elem);
 
+const moreObjects = [];
+
+import thumbnail from '../images/thumbnail-placeholder.png';
+
+let i = 0;
+for (let y = 100; y < 800; y += 200) {
+  for (let x = 100; x <= 1400; x += 200) {
+    moreObjects.push(new FindableObject(
+      `box${i}`,
+      `box${i}`,
+      thumbnail,
+      () => {
+        const path = new Path2D();
+        path.moveTo(x, y);
+        path.lineTo(x, y + 100);
+        path.lineTo(x + 100, y + 100);
+        path.lineTo(x + 100, y);
+        path.closePath();
+        return path;
+      },
+      { x: x - 50, y: y - 50 },
+      200
+    ));
+    i++;
+  }
+}
+
 root.render(
   <Game
-    buffer={true}
     container={elem} // image should always be wider than this element
     image={image}
     imageHeight={2448}
     imageWidth={1584}
-    objects={[Beanie, Football]}
+    // imageWidth={2448}
+    // imageHeight={1584}
+    // test={true}
+    objects={[
+      Beanie,
+      Football,
+      ...moreObjects
+    ]}
     onGameComplete={() => console.log('hooray!')}
   />
   // <Game
-  //   buffer={true}
   //   container={elem} // image should always be wider than this element
   //   image={'https://picsum.photos/800/700'}
   //   imageHeight={700}
