16390e808870d756ef4d66f3681427cd1a11d2bc
Jen Wachter <jwachter@jhu.edu>
2024-12-13 11:04:08 -0500

    Converted LegendScroll component to a class component

--- src/js/components/Legend/LegendScroll.js
+++ src/js/components/Legend/LegendScroll.js
@@ -1,109 +1,165 @@
-import React, { useEffect, useState } from 'react';
+/*global Modernizr*/
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ArrowIcon from '../Icons/Arrow';
 import ThumbnailGroups from './ThumbnailGroups';
 import * as settings from '../../../css/utils/shared-variables.scss';
+import './style.scss';
 
-export default function LegendScroll({ breakpoint, found, groups, legendWidth, thumbnailSize, width }) {
+require('../../lib/modernizr');
 
-  const [isPointerDown, setIsPointerDown] = useState(false);
-  const [direction, setDirection] = useState(null);
-  const [positionX, setPositionX] = useState(0);
+class LegendScrollComponent extends Component {
 
-  const buttonWidth = parseInt(settings.utilitiesIconHeight) + (parseInt(settings.buttonPadding) * 2);
-  const availableSpace = width - (buttonWidth * 2) - (parseInt(settings[`legendPadding_${breakpoint}`]) * 4);
+  constructor(props) {
+    super(props);
 
-  const minPositionX = 0;
-  const maxPositionX = -Math.abs(legendWidth - availableSpace);
+    this.isTouchEvents = Modernizr.touchevents;
+    this.intervalId = null;
 
-  if (positionX < maxPositionX) {
-    // on screen resize
-    setPositionX(maxPositionX);
-  }
+    const { availableSpace, minPositionX, maxPositionX } = this.getSpacingStates();
 
-  useEffect(() => {
-
-    let intervalId;
-
-    const scroll = () => {
-      setPositionX((prevPositionX) => {
-
-        const newValue = direction === 'left' ? prevPositionX + thumbnailSize : prevPositionX - thumbnailSize;
-
-        if (direction === 'left' && newValue >= minPositionX) {
-          setIsPointerDown(false);
-          return minPositionX;
-        } else if (direction === 'right' && newValue <= maxPositionX) {
-          setIsPointerDown(false);
-          return maxPositionX;
-        }
-
-        return newValue;
-      });
+    this.state = {
+      availableSpace,
+      minPositionX,
+      maxPositionX,
+      direction: null,
+      isPointerDown: false,
+      positionX: 0,
     }
 
-    if (isPointerDown) {
-      scroll();
-      intervalId = setInterval(scroll, 100);
-    }
-
-    return () => clearInterval(intervalId);
-
-  }, [direction, isPointerDown, maxPositionX, thumbnailSize]);
-
-  const handlePointerDown = direction => {
-    setIsPointerDown(true);
-    setDirection(direction);
-    window.addEventListener('pointerup', handlePointerUp, { once: true });
-  };
-
-  const handlePointerUp = () => {
-    setIsPointerDown(false);
+    this.getSpacingStates = this.getSpacingStates.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handlePointerDown = this.handlePointerDown.bind(this);
+    this.handlePointerUp = this.handlePointerUp.bind(this);
+    this.scrollViaButton = this.scrollViaButton.bind(this);
   }
 
-  const handleKeyDown = (e, direction) => {
+  getSpacingStates() {
+    let availableSpace = this.props.width;
+
+    if (!this.isTouchEvents) {
+      const buttonWidth = parseInt(settings.utilitiesIconHeight) + (parseInt(settings.buttonPadding) * 2);
+      availableSpace = this.props.width - (buttonWidth * 2) - (parseInt(settings[`legendPadding_${this.props.breakpoint}`]) * 4)
+    }
+
+    const minPositionX = 0;
+    const maxPositionX = -Math.abs(this.props.legendWidth - availableSpace);
+
+    console.log({width: this.props.width, availableSpace, maxPositionX});
+
+    return { availableSpace, minPositionX, maxPositionX };
+  }
+
+  handleKeyDown(e, direction) {
     if (e.key !== 'Enter') {
       return;
     }
-    handlePointerDown(direction)
+    this.handlePointerDown(direction)
   }
 
-  const scrollLeftDisabled = positionX === minPositionX;
-  const scrollRightDisabled = positionX === maxPositionX;
+  scrollViaButton(direction) {
+    this.setState(state => {
+      const newState = {};
+      const prevPositionX = state.positionX;
 
-  return (
-    <div className="legend-scroll">
-      <button
-        onKeyDown={(e) => handleKeyDown(e, 'left')}
-        onKeyUp={handlePointerUp}
-        onPointerDown={e => {
-          if (!scrollLeftDisabled) {
-            handlePointerDown('left')
-          }
-        }}
-        onPointerCancel={handlePointerUp}
-        disabled={scrollLeftDisabled}
-      >
-        <ArrowIcon className="left" tooltip="Scroll left" />
-      </button>
-      <ThumbnailGroups
-        found={found}
-        groups={groups}
-        position={positionX}
-        width={availableSpace}
-      />
-      <button
-        onKeyDown={(e) => handleKeyDown(e, 'right')}
-        onKeyUp={handlePointerUp}
-        onPointerDown={e => {
-          if (!scrollRightDisabled) {
-            handlePointerDown('right')
-          }
-        }}
-        onPointerCancel={handlePointerUp}
-        disabled={scrollRightDisabled}
-      >
-        <ArrowIcon className="right" tooltip="Scroll right" />
-      </button>
-    </div>
-  )
+      const newValue = direction === 'left' ? prevPositionX + this.props.thumbnailSize : prevPositionX - this.props.thumbnailSize;
+
+      if (direction === 'left' && newValue >= this.state.minPositionX) {
+        newState.pointerDown = false;
+        newState.positionX = this.state.minPositionX;
+      } else if (direction === 'right' && newValue <= this.state.maxPositionX) {
+        newState.pointerDown = false;
+        newState.positionX = this.state.maxPositionX;
+      } else {
+        newState.positionX = newValue;
+      }
+
+      return newState;
+    })
+  }
+
+  handlePointerDown(direction) {
+
+    this.setState({
+      isPointerDown: true,
+      direction
+    }, () => {
+
+      window.addEventListener('pointerup', this.handlePointerUp, { once: true });
+
+      this.scrollViaButton(direction);
+      this.intervalId = setInterval(() => {
+        console.log('button is being pressed');
+        this.scrollViaButton(direction);
+      }, 100);
+
+    });
+  };
+
+  handlePointerUp () {
+    clearInterval(this.intervalId);
+    this.setState({ isPointerDown: false });
+  }
+
+  render() {
+
+    const scrollLeftDisabled = this.state.positionX === this.state.minPositionX;
+    const scrollRightDisabled = this.state.positionX === this.state.maxPositionX;
+
+    return (
+      <div className="legend-scroll">
+        {!this.isTouchEvents &&
+          <button
+            onKeyDown={(e) => this.handleKeyDown(e, 'left')}
+            onKeyUp={this.handlePointerUp}
+            onPointerDown={e => {
+              if (!scrollLeftDisabled) {
+                this.handlePointerDown('left')
+              }
+            }}
+            onPointerCancel={this.handlePointerUp}
+            disabled={scrollLeftDisabled}
+          >
+            <ArrowIcon className="left" tooltip="Scroll left" />
+          </button>
+        }
+        <ThumbnailGroups
+          found={this.props.found}
+          groups={this.props.groups}
+          position={this.state.positionX}
+          width={this.state.availableSpace}
+        />
+        {!this.isTouchEvents &&
+          <button
+            onKeyDown={(e) => this.handleKeyDown(e, 'right')}
+            onKeyUp={this.handlePointerUp}
+            onPointerDown={e => {
+              if (!scrollRightDisabled) {
+                this.handlePointerDown('right')
+              }
+            }}
+            onPointerCancel={this.handlePointerUp}
+            disabled={scrollRightDisabled}
+          >
+            <ArrowIcon className="right" tooltip="Scroll right" />
+          </button>
+        }
+        
+      </div>
+    )
 }
+
+}
+
+LegendScrollComponent.defaultProps = {};
+
+LegendScrollComponent.propTypes = {
+  breakpoint: PropTypes.string.isRequired,
+  found: PropTypes.array.isRequired,
+  groups: PropTypes.array.isRequired,
+  width: PropTypes.number.isRequired,
+  legendWidth: PropTypes.number.isRequired,
+  thumbnailSize: PropTypes.number.isRequired,
+};
+
+export default LegendScrollComponent;
